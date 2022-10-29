a192d43a219403d5fb0537dcf1f0bdf59fd9c87a
Sourayaak <sourayaabdelkader@gmail.com>
2022-10-29 16:50:33 +0300

    Backend: User: Updating the functions which get an user information

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -1,35 +1,67 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\User;
-
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+use Symfony\Component\HttpFoundation\Response;
+use App\Models\User;
 
 class UserController extends Controller
 {
     public function getUsers(){ 
-        $users = User::where('user_type', 'user')->get(); ;
+        $users = User::where('user_type', 'user')->orderBy('created_at', 'DESC')->get(); ;
+       
+        if ($users->isNotEmpty()) {
+            return response()->json([
+                'data' => $users,
+                'message' => 'Found',
+                'status' =>  Response::HTTP_OK
+            ]);
+        }
+
         return response()->json([
-            "status" => "Success",
-            "data" => $users
+            'data' => null,
+            'message' => 'No Users',
+            'status' => Response::HTTP_OK
         ]);
     }
 
     public function getUserInfo($id){
         //$id= Auth::$id();
-        $user = User::where('id', $id)->get(); ;
+        $user = User::where('id', $id)->get();
+        if ($user->isNotEmpty()) {
+            return response()->json([
+                'data' => $user,
+                'message' => 'Found',
+                'status' =>  Response::HTTP_OK
+            ]);
+        }
+
         return response()->json([
-            "status" => "Success",
-            "data" => $user
+            'data' => null,
+            'message' => 'User Not Found',
+            'status' => Response::HTTP_OK
         ]);
     }
 
     public function getUserByEmail($email){
         //$user = Auth::user();
         $user = User::where('email', $email)->get(); ;
+        if ($user->isNotEmpty()) {
+            return response()->json([
+                'data' => $user,
+                'message' => 'Found',
+                'status' =>  Response::HTTP_OK
+            ]);
+        }
+
         return response()->json([
-            "status" => "Success",
-            "data" => $user
+            'data' => null,
+            'message' => 'User Not Found',
+            'status' => Response::HTTP_OK
         ]);
     }
+
+
 }
