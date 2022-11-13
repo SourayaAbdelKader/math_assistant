20fe687c891029a4ca01f87de451b53b9320f97d
Sourayaak <sourayaabdelkader@gmail.com>
2022-11-14 00:56:43 +0200

    Frontend: Components: Adding the header component

---
+++ frontend/src/Components/Headers/Headers.js
@@ -0,0 +1,34 @@
+import React from "react";
+import {Routes, Route, useNavigate} from 'react-router-dom';
+
+// Importing styling and assets
+import './headers.css';
+import logo from '../../images/logo.png';
+import notification from '../../images/notification.png';
+import profile from '../../images/profile.png';
+
+// Importing components
+import Signout from "../Buttons/Signout";
+
+const Header = () => {
+    
+    const navigate = useNavigate();
+    const navigateProfile = () => {navigate('/profile');};
+    const navigateHome = () => {navigate('/');};
+
+    return(
+        <div className="headercontainer">
+            <div className="flex">
+                <div> <img className="pointer" onClick={navigateHome} src={logo} alt="logo" /> </div>
+                <div> <h3 className="white_text pointer" onClick={navigateHome}> Math Assistant </h3> </div>
+            </div>
+            <div className="flex"> 
+                <div> <img className="icon cursor" src={notification} alt="notification"/> </div>
+                <div> <img className="icon cursor" onClick={navigateProfile} src={profile} alt="profile"/> </div>
+                <Signout></Signout>
+            </div>
+        </div>    
+    )
+}
+
+export default Header;
