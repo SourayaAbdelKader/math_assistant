841516ec51f1b3d4619b22afcc0bd37feceaa8b1
Sourayaak <sourayaabdelkader@gmail.com>
2022-11-17 18:28:32 +0200

    Frontend: Components: Adding sidebar component for the solved problems page

---
+++ frontend/src/Components/Sidebars/SolvedProblemsSidebar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './sidebars.css';
+
+const SolvedProblemsSidebar = () => {
+    return(
+        <div className="side_container">
+            <div> <h3> Solved Problems </h3> </div>
+        </div>        
+    )
+}
+
+export default SolvedProblemsSidebar;
