811973204c469fb64b71a89666faa4b6efb4e8df
Sourayaak <sourayaabdelkader@gmail.com>
2022-11-17 18:34:23 +0200

    Frontend: Components: Cards: Adding the problem card

---
+++ frontend/src/Components/Cards/ProblemCard.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+// Importing style
+import './cards.css';
+
+// Importing buttons
+import Solve from '../Buttons/Solve';
+
+const ProblemCard = (props) => {    
+    return(
+        <div onClick={props.onClick} id={props.id} className="problem_card flex"> 
+            <div> 
+                <div> <h4 className="question_title"> Question {props.id} </h4> </div>
+                <div> <p className="question_name"> {props.name} </p> </div>
+            </div>
+            <div> <Solve></Solve> </div>
+        </div>
+    )
+}
+
+export default ProblemCard;
