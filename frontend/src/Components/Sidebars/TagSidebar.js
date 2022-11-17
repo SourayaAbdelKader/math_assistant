44a032f101fe6cc4220e2674d460026691eceed6
Sourayaak <sourayaabdelkader@gmail.com>
2022-11-17 18:29:23 +0200

    Frontend: Components: Adding sidebar component for the tags page

---
+++ frontend/src/Components/Sidebars/TagSidebar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './sidebars.css';
+
+const TagSidebar = () => {
+    return(
+        <div className="side_container">
+            <div> <h3> Tags </h3> </div>
+        </div>        
+    )
+}
+
+export default TagSidebar;
