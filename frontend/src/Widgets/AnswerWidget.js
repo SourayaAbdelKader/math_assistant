95aea2a0c7a48ccce4f93fd7ddd143bbc52963b5
Sourayaak <sourayaabdelkader@gmail.com>
2022-11-18 23:58:09 +0200

    Frontend: Connecting the voting to the frontend

--- frontend/src/Widgets/AnswerWidget.js
+++ frontend/src/Widgets/AnswerWidget.js
@@ -8,6 +8,8 @@ import accepted_image from '../images/accepted.png';
 import picture from '../images/profileSelected.png';
 import vote_up from '../images/vote_up.png';
 import vote_down from '../images/vote_down.png';
+import voted_up from '../images/voted_up.png';
+import voted_down from '../images/vote_down.png';
 
 // Importing popup
 import Popup from 'reactjs-popup';
@@ -21,8 +23,9 @@ import AnswerAPI from '../hooks/answersApi';
 const AnswerWidget = (answer) => {
 
     const componentRef = React.useRef();
-
+    const [openAccept, setOpenAccept] = useState(false);
     const [voteUp, setVoteUp] = useState("");
+    const [voteDown, setVoteDown] = useState("");
     const [message, setMessage] = useState('');
   
     const handleClick = (e) => {
@@ -40,10 +43,12 @@ const AnswerWidget = (answer) => {
         e.preventDefault();
         if (answer.accepted == 0){
             acceptAnAnswer(localStorage.getItem('user_id'), answer.id)
-            if (message == 'Added Successfully'){
+            if (acceptAnAnswer(localStorage.getItem('user_id'), answer.id) == 'Added Successfully'){
                 e.target.src = accepted_image;
-            }  
+            }
+            setOpenAccept(true);  
         }
+        //setMessage('') 
     }
 
     const acceptAnAnswer = async (user_id, answer_id) => {
@@ -51,11 +56,49 @@ const AnswerWidget = (answer) => {
             "user_id":user_id,
             "answer_id":answer_id,
         });
-        console.log(accept_answer.data.message)
-        setMessage(accept_answer.data.message);    
+        console.log(accept_answer.data.message);
+        setMessage(accept_answer.data.message);
+        return  accept_answer.data.message;   
     }
-    console.log(message.length)
+
     const image_source = handleAcceptance();
+
+    const handleVote = (e) =>{
+        e.preventDefault();
+        voteUpAnAnswer(localStorage.getItem('user_id'), answer.id);
+        if (message === 'Added Successfully'){
+            e.target.src = voted_up
+        }
+    }
+
+    const voteUpAnAnswer = async (user_id, answer_id) => {
+        const accept_answer = await AnswerAPI.voteUpAnswer({
+            "user_id":user_id,
+            "answer_id":answer_id,
+            "vote":1
+        });
+        console.log(accept_answer.data.message)
+        setVoteUp(accept_answer.data.message);  
+        return accept_answer.data.message  
+    }
+
+    const handleVoteDown = (e) => {
+        e.preventDefault();
+        voteDownAnAnswer(localStorage.getItem('user_id'), answer.id);
+        if (message == 'Added Successfully'){
+            e.target.src = voted_down
+        }
+    }
+
+    const voteDownAnAnswer = async (user_id, answer_id) => {
+        const accept_answer = await AnswerAPI.voteDownAnswer({
+            "user_id":user_id,
+            "answer_id":answer_id,
+            "vote":0
+        });
+        console.log(accept_answer.data.message)
+        setVoteDown(accept_answer.data.message);    
+    }
     
     return(
         <div onClick={handleClick} id={answer.id} className="answer_container">
@@ -67,6 +110,16 @@ const AnswerWidget = (answer) => {
                 <div>
                 <div>
                     <img onClick={handleAccept} className="medium_icon cursor" src={image_source} alt="accept" />
+                    <Popup open={openAccept} modal nested >
+                                        {close => (
+                                        <div className="modal">
+                                            <button className="close" onClick={close}>
+                                            &times;
+                                            </button>
+                                            <div> <h3>{message}</h3></div>
+                                        </div>
+                                        )}
+                        </Popup>
                 </div>
                 </div>
             </div>
@@ -76,8 +129,8 @@ const AnswerWidget = (answer) => {
                 </div>
             </div>
             <div className="flex_end">
-                <div> <img className="medium_icon cursor" src={vote_down} alt="save" /> </div>
-                <div> <img className="medium_icon cursor space_left" src={vote_up} alt="save" /> </div>
+                <div> <img onClick={handleVoteDown}  className="medium_icon cursor" src={vote_down} alt="save" /> </div>
+                <div> <img onClick={handleVote} className="medium_icon cursor space_left" src={vote_up} alt="save" /> </div>
             </div>
         </div>        
     )
