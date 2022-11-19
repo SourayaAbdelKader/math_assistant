21708fff2fa4a65a9b1701abaec8af764e5e0c79
Sourayaak <sourayaabdelkader@gmail.com>
2022-11-19 12:54:43 +0200

    Frontend: Adding the navbar

--- admin/src/Pages/Dashboard.js
+++ admin/src/Pages/Dashboard.js
@@ -3,19 +3,66 @@ import './pages.css'
 
 // Importing Components
 import LowerFooter from '../Components/LowerFooter';
-import LoginHeader from '../Components/Headers/LoginHeader';
+import Header from '../Components/Headers/Header';
+import Navbar from '../Components/Navbar/Navbar';
+import { Chart } from "react-google-charts";
 
 const Dashboard = () => {
+     const data = [
+        ["Task", "Hours per Day"],
+        ["User", 11],
+        ["admin", 2],
+        ["editor", 2],
+      ];
+      
+    const options = {
+        title: "User distributy",
+      };
+
+    const data2 = [
+        ["Year", "Sales", "Expenses"],
+        ["2014", 1000, 400],
+        ["2015", 1170, 460],
+        ["2016", 660, 1120],
+        ["2017", 1030, 540],
+      ];
+
+    const options2 = {
+        chart: {
+          title: "Performance",
+          subtitle: "",
+        },
+      };
 
     return (
         <div>
-            <LoginHeader></LoginHeader>
+            <Header></Header>
+            <div className='page_content'>
+                <div className='navbar'> <Navbar></Navbar></div>
+                <div className='content'>
                 <section>
-                   <div className='hero'> 
-                        <h3 className="white_text space"> Math Assistant</h3>
-                        <p className='white_text'> Description right here </p>
-                    </div>
+                    {/* <div>     
+                        <Chart
+                        chartType="PieChart"
+                        data={data}
+                        options={options}
+                        width={"100%"}
+                        height={"400px"}
+                        />
+                </div>
+                <div>
+                    <Chart
+                    chartType="Bar"
+                    width="50%"
+                    height="20px"
+                    data={data2}
+                    options={options2}
+                    />
+                </div> */}
                 </section>
+
+                </div> 
+            </div>
             <LowerFooter></LowerFooter>
         </div>
     );
