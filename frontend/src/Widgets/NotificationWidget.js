dbf955005f1598298c07d9b21ec1121de836c893
Sourayaak <sourayaabdelkader@gmail.com>
2022-11-22 23:22:54 +0200

    Frontend: Notification: Adjusting a function in the notification item

--- frontend/src/Widgets/NotificationWidget.js
+++ frontend/src/Widgets/NotificationWidget.js
@@ -16,13 +16,14 @@ import {  onMessageListener, getTokens } from '../firebase';
 import { getMessaging, onMessage, getToken } from "firebase/messaging";
 import UserAPI from '../hooks/userAPI';
 
-
 const NotificationWidget = (answer) => {
     const navigate = useNavigate();
     const [notification, setNotification] = useState({title: '', body: ''});
     const [image, setImage] = useState(empty_notification);
     const [messages, setMessages] = useState([]);
     const [isTokenFound, setTokenFound] = useState(false);
+    const [empty, setEmpty] = useState(false);
+
     useEffect(() =>{
         const sendDeviceToken  = async () =>{
             const token = await UserAPI.deviceToken({
@@ -30,8 +31,6 @@ const NotificationWidget = (answer) => {
             "device_token":localStorage.getItem("device_token"),
         });}
 
-        
-    
         const getNotifications  = async () =>{
             const notification = await UserAPI.getNotificationForUser(localStorage.getItem('user_id'));
             console.log(notification.data.data)
@@ -43,23 +42,13 @@ const NotificationWidget = (answer) => {
                         setNotification({title: payload.notification.title, body: payload.notification.body})
                 }).catch(err => console.log('failed: ', err));
                 const push_notification = await UserAPI.sendNotification({
-                    "id": 15,
+                    "id": 15, // the id is hard coded because it's the device id on which i'm testing the push notification
                     "title": "Math Assistant",
                     "body": 'You got a new notification.',
                 });
-            }
+            } else {setEmpty(true)}
         };
     ; sendDeviceToken(); getNotifications();}, []);
-
-    const componentRef = React.useRef();
-
-    const seeFeedback = (e) => {
-        // navigate('/practice/feedback');
-        // localStorage.setItem('selected_practice', )
-        // localStorage.setItem('', )
-        console.log(e.target.id)
-        
-    }
     
     return(
         <div>
@@ -71,6 +60,9 @@ const NotificationWidget = (answer) => {
                                 <NotificationItem key={e.id} id={e.id} title={e.title} body={e.body}> </NotificationItem>
                             )   
                         }) 
+                    }
+                    {
+                        empty && (<div> <p> No new notifications </p> </div>)
                     }
                 </div>
             </Popup>
